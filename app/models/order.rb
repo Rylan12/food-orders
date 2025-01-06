dd0b17ff638d953c78cd1d5e8212a21c869eb2a3
Rylan Polster <rslpolster@gmail.com>
2025-01-06 18:16:23 -0500

    Add necessary dependent destroy lines

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :person
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :food_items, through: :order_items
 
   enum :status, { pending: 0, placed: 1, received: 2 }
