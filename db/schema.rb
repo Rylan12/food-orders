e9984b17bf88eb9255106fb59312e5ed40f630d4
Rylan Polster <rslpolster@gmail.com>
2025-01-06 17:41:44 -0500

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_06_223829) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_06_224112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "food_items", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name", null: false
