96d0170e600b0eab56658176b6ca9aaa4819fdae
Rylan Polster <rslpolster@gmail.com>
2025-01-06 17:56:41 -0500

    Generate OrderItem model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :person
+  has_many :order_items
+  has_many :food_items, through: :order_items
+
   enum :status, { pending: 0, placed: 1, received: 2 }
 end
